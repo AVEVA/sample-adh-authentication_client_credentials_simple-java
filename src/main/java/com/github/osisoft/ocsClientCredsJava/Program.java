9b0bf41cfff8aea3a393c08836a00bfb93c5fbf3
Kelsey Duffy <kduffy@osisoft.com>
2022-02-09 09:10:30 -0500

    Initial release (#1)

---
+++ src/main/java/com/github/osisoft/ocsClientCredsJava/Program.java
@@ -0,0 +1,97 @@
+package com.github.aveva.adhClientCredsJava;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class Program {
+    // Step 1: get needed variables 
+    static Appsettings appsettings = getAppsettings();
+
+    static String tenantId = appsettings.getTenantId();
+    static String apiVersion = appsettings.getApiVersion();
+    static String resource = appsettings.getResource();
+    static String clientId = appsettings.getClientId();
+    static String clientSecret = appsettings.getClientSecret();
+
+    public static void main(String[] args) throws IOException {
+        toRun();
+    }
+
+    public static boolean toRun() throws IOException {
+        // Step 2: get the authentication endpoint from the discovery URL
+        URL discoveryUrl = new URL(resource + "/identity/.well-known/openid-configuration");
+        URLConnection request = discoveryUrl.openConnection();
+        request.connect();
+        JsonObject rootObj = JsonParser
+                .parseReader(new InputStreamReader((InputStream) request.getContent(), StandardCharsets.UTF_8))
+                .getAsJsonObject();
+        String tokenUrl = rootObj.get("token_endpoint").getAsString();
+
+        // Step 3: use the client ID and Secret to get the needed bearer token
+        String accessToken = null;
+        URL token = new URL(tokenUrl);
+        HttpURLConnection tokenRequest = (HttpURLConnection) token.openConnection();
+        tokenRequest.setRequestMethod("POST");
+        tokenRequest.setRequestProperty("Accept", "application/json");
+        tokenRequest.setDoOutput(true);
+        tokenRequest.setDoInput(true);
+
+        String postString = "client_id=" + URLEncoder.encode(clientId, "UTF-8") + "&client_secret="
+                + URLEncoder.encode(clientSecret, "UTF-8") + "&grant_type=client_credentials";
+        byte[] postData = postString.getBytes("UTF-8");
+        tokenRequest.setRequestProperty("Content-Length", Integer.toString(postData.length));
+        try (OutputStream stream = tokenRequest.getOutputStream()) {
+            stream.write(postData);
+        }
+
+        String result;
+        try (InputStream in = new BufferedInputStream(tokenRequest.getInputStream())) {
+            result = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
+        }
+
+        JsonObject response = JsonParser.parseString(result).getAsJsonObject();
+        accessToken = response.get("access_token").getAsString();
+
+        // Step 4: test token by calling the base tenant endpoint
+        URL tenant = new URL(resource + "/api/" + apiVersion + "/Tenants/" + tenantId);
+        HttpURLConnection tenantRequest = (HttpURLConnection) tenant.openConnection();
+        tenantRequest.setRequestProperty("Authorization", "Bearer " + accessToken);
+        tenantRequest.connect();
+
+        // return whether the status code was 200 OK in order to test the function
+        return tenantRequest.getResponseCode() == 200;
+    }
+
+    private static Appsettings getAppsettings() {
+        Gson mGson = new Gson();
+
+        Appsettings appsettings = new Appsettings();
+
+        try (InputStream inputStream = new FileInputStream("appsettings.json")) {
+            String fileString = null;
+            int bytesToRead = inputStream.available();
+
+            if (bytesToRead > 0) {
+                byte[] bytes = new byte[bytesToRead];
+                inputStream.read(bytes);
+                fileString = new String(bytes);
+            }
+
+            appsettings = mGson.fromJson(fileString, Appsettings.class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return appsettings;
+    }
+}
---
+++ src/test/java/com/github/osisoft/ocsClientCredsJava/ProgramTest.java
@@ -0,0 +1,18 @@
+/** ProgramTest.java
+ * 
+ */
+package com.github.aveva.adhClientCredsJava;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for simple App.
+ */
+public class ProgramTest {
+    @Test
+    public void runMainProgram() throws java.io.IOException {
+        assertTrue(Program.toRun());
+    }
+}
